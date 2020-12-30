308400f3d241a8efaf4a4ea0a698bac87ebebdf0
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2020-12-30 09:21:16 +0700

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            for (i = 0; i < 5; i++) 
+            {
+                for (j = 0; j <= i; j++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Test2
+    {
+        static void Main(string[] args)
+        {
+            int grade;
+            Console.WriteLine("What is your grade?");
+            grade = Convert.ToInt32(Console.ReadLine());
+            if (grade >= 40)
+            {
+                Console.WriteLine("U pass");
+            }
+            else
+            {
+                Console.WriteLine("U failed");
+            }
+            if (grade >= 75 && grade < 80)
+            {
+                Console.WriteLine("$1000");
+            }
+            else if (90 > grade && grade >= 80 )
+            {
+                Console.WriteLine("1500");
+            }
+            else if (grade >= 90)
+            {
+                Console.WriteLine("$2000");
+            }
+            Console.ReadLine();
+        }
+    }
+}
